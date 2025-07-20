e2c4c89bd302d2395d5a324a87bf691b3add95e3
Fujimoto Seiji <fujimoto@ceptord.net>
2025-07-20 11:23:46 +0900

    Add the first cut at backend processing
    This starts using the backend API to perform the tensor computation,
    in place of the context API.
    
    It already reaches the point where it produces a correct result, but
    surely needs more closer examination.
    
    Signed-off-by: Fujimoto Seiji <fujimoto@ceptord.net>

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 COPTS  = -O2
 
-all: matmul conv1d
+all: matmul conv1d conv1d_backend
 
 matmul: matmul.c
 	$(CC) $(COPTS) -I ggml/include -L ggml/build/src -o matmul matmul.c -l ggml-base -l ggml-cpu -l ggml
@@ -8,8 +8,12 @@ matmul: matmul.c
 conv1d: conv1d.c
 	$(CC) $(COPTS) -I ggml/include -L ggml/build/src -o conv1d conv1d.c -l ggml-base -l ggml-cpu -l ggml
 
+conv1d_backend: conv1d_backend.c
+	$(CC) $(COPTS) -I ggml/include -L ggml/build/src -o conv1d_backend conv1d_backend.c -l ggml-base -l ggml-cpu -l ggml
+
 clean:
 	rm -f matmul
 	rm -f conv1d
+	rm -f conv1d_backend
 
 .PHONY: all clean
---
+++ conv1d_backend.c
@@ -0,0 +1,49 @@
+#include <ggml.h>
+#include <ggml-cpu.h>
+#include <ggml-backend.h>
+#include <string.h>
+#include <inttypes.h>
+
+int main(int argc, char **argv)
+{
+    ggml_backend_t backend = ggml_backend_cpu_init();
+    ggml_backend_buffer_t buffer;
+
+    struct ggml_init_params params = {
+        1024 * 1024 * 1024, /* 1 MB */
+        NULL,
+        true
+    };
+
+    struct ggml_context *ctx = ggml_init(params);
+    struct ggml_cgraph *gf = ggml_new_graph(ctx);
+
+    /*
+     * This applies an 1D convolution over an input tensor
+     * {1, 2, 3, 4, 5} using a kernel tensor {3, 4, 5}.
+     */
+    struct ggml_tensor *tA = ggml_arange(ctx, 1, 6, 1);
+
+    struct ggml_tensor *tK_f32 = ggml_arange(ctx, 3, 6, 1);
+    struct ggml_tensor *tK = ggml_cast(ctx, tK_f32, GGML_TYPE_F16);
+
+    struct ggml_tensor *tR = ggml_conv_1d(ctx, tK, tA, 1, 1, 1);
+    ggml_build_forward_expand(gf, tR);
+
+    buffer = ggml_backend_alloc_ctx_tensors(ctx, backend);
+
+    ggml_backend_cpu_set_n_threads(backend, 2);
+    ggml_backend_graph_compute(backend, gf);
+
+    /*
+     * The result must be {14, 26, 38, 50, 32}.
+     */
+    printf("[");
+    for (int i = 0; i < ggml_nelements(tR); i++) {
+        printf(" %-5.1f", ((float *) tR->data)[i]);
+    }
+    printf("]\n");
+
+    ggml_backend_buffer_free(buffer);
+    ggml_backend_free(backend);
+}
