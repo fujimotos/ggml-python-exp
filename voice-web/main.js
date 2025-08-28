7e611f3dc16b8699f344cba163c66a07bf4969ae
Fujimoto Seiji <fujimoto@ceptord.net>
2025-08-28 10:07:46 +0900

    Enable users to upload their audio files
    In particular, this adds an <input> element that accepts local
    files.
    
    This also cleans up the initialization sequence of the app, so
    that we can reload the application state cleanly on each file.
    
    Signed-off-by: Fujimoto Seiji <fujimoto@ceptord.net>

--- voice-web/main.js
+++ voice-web/main.js
@@ -29,12 +29,12 @@ function save_to_storage(ctx)
 
 function load_from_storage(ctx)
 {
+    ctx.region_plugin.clearRegions();
+
     let blob = localStorage.getItem(ctx.filename);
     if (!blob) {
         return;
     }
-
-    ctx.region_plugin.clearRegions();
 
     let dict = JSON.parse(blob);
 
@@ -44,6 +44,17 @@ function load_from_storage(ctx)
             end: vs.end,
         });
     }
+}
+
+function open_new_audio(ctx, file)
+{
+    ctx.loading = true;
+    ctx.wavesurfer.loadBlob(file).then(() => {
+        ctx.filename = file.name;
+        load_from_storage(ctx);
+        update_textarea(ctx);
+        ctx.loading = false;
+    });
 }
 
 function update_textarea(ctx)
@@ -69,47 +80,6 @@ function set_active_region(ctx, new_region)
     }
 }
 
-function start_app(ctx)
-{
-    ctx.textarea = document.querySelector('#label');
-
-    const upload = document.querySelector('input[type="file"]');
-
-    upload.addEventListener("input", (e) => {
-        let file = e.target.files[0];
-        ctx.wavesurfer.loadBlob(file);
-        ctx.filename = file.name;
-
-        load_from_storage(ctx);
-        console.log(upload);
-        e.target.blur();
-    });
-
-    const slider = document.querySelector('input[type="range"]');
-
-    slider.addEventListener("input", (e) => {
-        ctx.wavesurfer.zoom(Number(e.target.value))
-    });
-
-    ctx.wavesurfer.zoom(slider.value);
-
-    window.addEventListener('keydown', (e) => {
-        if (e.key == ' ') {
-            if (ctx.active_region) {
-                ctx.active_region.play(true);
-            } else if (ctx.wavesurfer.isPlaying()) {
-                ctx.wavesurfer.pause();
-            } else {
-                ctx.wavesurfer.play();
-            }
-            e.preventDefault();
-        }
-    })
-
-    load_from_storage(ctx);
-    update_textarea(ctx);
-}
-
 /*
  * Main
  */
@@ -121,6 +91,9 @@ function main()
         active_region: null,
         filename: null,
         textarea: null,
+        fileinput: null,
+        slider: null,
+        loading: false,
     };
 
     ctx.region_plugin = RegionsPlugin.create();
@@ -128,22 +101,22 @@ function main()
     ctx.wavesurfer = WaveSurfer.create({
         container: '#waveform',
         waveColor: COLOR_WAVE,
-        url: '/sample.mp3',
         plugins: [ctx.region_plugin],
         barWidth: 2,
         barGap: 1,
         barRadius: 2,
     });
-    ctx.filename = 'sample.mp3';
 
     ctx.region_plugin.enableDragSelection({
         color: COLOR_REGION_INACTIVE,
     });
 
     ctx.region_plugin.on('region-created', (region) => {
-        set_active_region(ctx, region);
-        save_to_storage(ctx);
-        update_textarea(ctx);
+        if (!ctx.loading) {
+            set_active_region(ctx, region);
+            save_to_storage(ctx);
+            update_textarea(ctx);
+        }
     });
 
     ctx.region_plugin.on('region-updated', (region) => {
@@ -159,8 +132,8 @@ function main()
 
     ctx.region_plugin.on('region-double-clicked', (region, e) => {
         e.stopPropagation();
-        if (region == ctx.active_region) {
-          clear_active_region(ctx);
+        if (region === ctx.active_region) {
+            clear_active_region(ctx);
         }
         region.remove();
         save_to_storage(ctx);
@@ -171,9 +144,48 @@ function main()
         clear_active_region(ctx);
     })
 
-    ctx.wavesurfer.once('decode', () => {
-      start_app(ctx);
+    ctx.wavesurfer.on('decode', () => {
+        ctx.wavesurfer.zoom(ctx.slider.value);
     })
+
+    /*
+     * DOM Events
+     */
+    window.addEventListener('keydown', (e) => {
+        if (e.key == ' ') {
+            if (ctx.active_region) {
+                ctx.active_region.play(true);
+            } else if (ctx.wavesurfer.isPlaying()) {
+                ctx.wavesurfer.pause();
+            } else {
+                ctx.wavesurfer.play();
+            }
+            e.preventDefault();
+        }
+    })
+
+    window.addEventListener("DOMContentLoaded", () => {
+        ctx.textarea = document.querySelector('#label');
+        ctx.fileinput = document.querySelector('input[type="file"]');
+        ctx.slider = document.querySelector('input[type="range"]');
+
+        ctx.slider.addEventListener("input", (e) => {
+            ctx.wavesurfer.zoom(Number(e.target.value))
+        });
+
+        ctx.fileinput.addEventListener("input", (e) => {
+            e.target.blur();
+            open_new_audio(ctx, ctx.fileinput.files[0]);
+        });
+
+        /*
+         * This is for F5-key reload, Browsers can preserve
+         * the state of the file input.
+         */
+        if (ctx.fileinput.files.length > 0) {
+            open_new_audio(ctx, ctx.fileinput.files[0]);
+        }
+    });
 };
 
 main();
